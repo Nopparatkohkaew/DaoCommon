af77a381eb1f60aa51bd2e1c72da7ab4392f2205
Supakoa <41123305+Supakoa@users.noreply.github.com>
2021-01-15 16:58:31 +0700

    [koa] add pathMac

--- src/main/java/utils/Contains.java
+++ src/main/java/utils/Contains.java
@@ -4,8 +4,11 @@ public class Contains {
     public static final String customer = "DBCustomer";
     public static final String policy = "DBPolicy";
     public static final String pathL = "/c/resources/db/";
+    public static final String pathMac = "/Users/spinkk/Documents/GitHub/Programmer_jobs/Thai-Life-Insurance//c/resources/db/";
     public static final String key = "dbkey.cfg";
     public static final String config = "dbconfig.cfg";
     public static final String pathKeyLinux = pathL.concat(key);
     public static final String pathConfigLinux = pathL.concat(config);
+    public static final String pathKeyMac = pathMac.concat(key);
+    public static final String pathConfigMac = pathMac.concat(config);
 }
